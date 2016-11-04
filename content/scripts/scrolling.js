160cfbeac636b82d9fcedd2dcf824d0396195a3a
davidfeltendal <davidfeltendal@gmail.com>
2016-11-04 13:04:48 +0100

    Social media link + logo

--- content/scripts/scrolling.js
+++ content/scripts/scrolling.js
@@ -15,15 +15,4 @@ $(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
-    // init NanoGallery
-    $(document).ready(function () {
-        $("#nanoGallery3").nanoGallery({
-            thumbnailWidth: 'auto',
-            thumbnailHeight: 100,
-            locationHash: false,
-            thumbnailHoverEffect: 'borderLighter,imageScaleIn80',
-            itemsBaseURL: 'http://nanogallery.brisbois.fr/demonstration/'
-        });
-    });
-
 });
